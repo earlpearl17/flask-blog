e80ed3da3e57efa880ddd0d2a2bd1d4d0534a8f9
Earl Pearl <earlpearl17@gmail.com>
2020-08-10 05:41:51 -0400

    Pre Deployment

--- blog.py
+++ blog.py
@@ -1,6 +1,7 @@
 # blog.py - controller
 
 # imports
+import os
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
@@ -80,4 +81,7 @@ def logout():
     return redirect(url_for('login'))    
 
 if __name__ == '__main__':
-    app.run(debug=True)    
+    #app.run(debug=True)    
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
+    app.run()
